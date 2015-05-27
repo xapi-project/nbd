256780cbe9206acac0f18cb6e3c95ddab2d74775
David Scott <dave.scott@eu.citrix.com>
2015-05-27 14:37:21 +0100

    Move the SERVER signature into s.mli
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- lib/s.mli
+++ lib/s.mli
@@ -30,3 +30,31 @@ module type CLIENT = sig
       performs the initial protocol negotiation and connects to
       the named export. Returns (remote disk size * flags) *)
 end
+
+module type SERVER = sig
+
+  type t
+  (** An open connection to an NBD client *)
+
+  type size = int64
+  (** The size of a remote disk *)
+
+  type name = string
+  (** The name of an export. In the 'new style' protocol as used in nbd >= 2.9.17
+      the client must select an export by name. *)
+
+  val connect : channel -> ?offer:name list -> unit -> (name * t) Lwt.t
+  (** [connect channel ?offer ()] performs the 'new style' initial handshake
+      and options negotiation. If ?offer is provided then these names will be returned
+      if the client requests a list of exports, otherwise we will return EPERM.
+      The client's choice of name is returned which must be looked up by the
+      application. If the name is invalid, the only option is to close the connection.
+      If the name is valid then use the [serve] function. *)
+
+  val serve : t ->  (module V1_LWT.BLOCK with type t = 'b) -> 'b -> unit Lwt.t
+  (** [serve t block b] runs forever processing requests from [t], using [block]
+      device type [b]. *)
+
+  val close: t -> unit Lwt.t
+  (** [close t] shuts down the connection [t] and frees any allocated resources *)
+end
