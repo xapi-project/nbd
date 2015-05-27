1693025a0c347592088a9f40a2116a95e67a1226
David Scott <dave.scott@eu.citrix.com>
2015-05-27 14:35:29 +0100

    Start adding common signatures to s.mli
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- lib/client.mli
+++ lib/client.mli
@@ -11,20 +11,5 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *)
-open Channel
 
-include V1_LWT.BLOCK
-
-type size = int64
-(** The size of a remote disk *)
-
-val list: channel -> [ `Ok of string list | `Error of [ `Policy | `Unsupported ] ] Lwt.t
-(** [list channel] returns a list of exports known by the server.
-    `Error `Policy means the server has this function disabled deliberately.
-    `Error `Unsupported means the server is old and does not support the query
-    function. *)
-
-val negotiate: channel -> string -> (t * size * Protocol.PerExportFlag.t list) Lwt.t
-(** [negotiate channel export] takes an already-connected channel,
-    performs the initial protocol negotiation and connects to
-    the named export. Returns (remote disk size * flags) *)
+include S.CLIENT
---
+++ lib/s.mli
@@ -0,0 +1,32 @@
+(*
+ * Copyright (C) Citrix Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *)
+open Channel
+
+module type CLIENT = sig
+  include V1_LWT.BLOCK
+
+  type size = int64
+  (** The size of a remote disk *)
+
+  val list: channel -> [ `Ok of string list | `Error of [ `Policy | `Unsupported ] ] Lwt.t
+  (** [list channel] returns a list of exports known by the server.
+      `Error `Policy means the server has this function disabled deliberately.
+      `Error `Unsupported means the server is old and does not support the query
+      function. *)
+
+  val negotiate: channel -> string -> (t * size * Protocol.PerExportFlag.t list) Lwt.t
+  (** [negotiate channel export] takes an already-connected channel,
+      performs the initial protocol negotiation and connects to
+      the named export. Returns (remote disk size * flags) *)
+end
