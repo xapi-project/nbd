327d87967b14231e66fa373f2df38e30eb1668ab
David Scott <dave.scott@eu.citrix.com>
2015-05-29 14:54:57 +0100

    mirror: use a single background copy and immediate synchronous mirroring
    This avoids tracking a dirty list and only requires the ability to
    lock a disk region for mirroring, preventing new data being written there
    which might be overwritten on the secondary with the old data.
    
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- lib/mirror.ml
+++ lib/mirror.ml
@@ -37,16 +37,139 @@ module Make(Primary: V1_LWT.BLOCK)(Secondary: V1_LWT.BLOCK) = struct
     size_sectors: int64;
   }
 
+  module Region_lock = struct
+    (* We need to prevent the background mirror thread racing with an I/O write
+       to a particular region *)
+
+    type region = int64 * int
+
+    let overlap (start, length) (start', length') =
+      start' >= start && (start' < Int64.(add start (of_int length)))
+
+    let before (start, length) (start', length') =
+      Int64.(add start (of_int length)) < start'
+
+    type t = {
+      mutable exclusive_lock: region; (* extent we're currently copying *)
+      mutable active: region list; (* extents which are being written to *)
+      c: unit Lwt_condition.t;
+      m: Lwt_mutex.t;
+    }
+
+    (* Acquire an exclusive lock on [offset:offset+length]. While this lock
+       is held, other threads will block. *)
+    let with_exclusive_lock t offset length f =
+      Lwt_mutex.with_lock t.m
+        (fun () ->
+          let rec wait () =
+            if List.fold_left (||) false (List.map (overlap (offset, length)) t.active) then begin
+              Lwt_condition.wait ~mutex:t.m t.c
+              >>= fun () ->
+              wait ()
+            end else return () in
+          wait ()
+          >>= fun () ->
+          t.exclusive_lock <- (offset, length);
+          return ()
+        )
+      >>= fun () ->
+      let unlock () =
+        Lwt_mutex.with_lock t.m
+          (fun () ->
+            t.exclusive_lock <- (-1L, 0);
+            Lwt_condition.broadcast t.c ();
+            return ()
+          ) in
+      Lwt.catch
+        (fun () ->
+          f () >>= fun r ->
+          unlock () >>= fun () ->
+          return r)
+        (fun e ->
+          unlock () >>= fun () ->
+          fail e)
+
+    (* Exclude the background copying thread from [offset:offset+length]. This avoids updating
+       a region while it is being actively mirrored, which could cause the old data
+       to overtake and overwrite the new data. *)
+    let with_lock t offset length f =
+      Lwt_mutex.with_lock t.m
+        (fun () ->
+          let rec loop () =
+            if overlap t.exclusive_lock (offset, length) then begin
+              Lwt_condition.wait ~mutex:t.m t.c
+              >>= fun () ->
+              loop ()
+            end else begin
+              (* if the copy might catch up with us then mark the region as locked *)
+              let unlock =
+                if before t.exclusive_lock (offset, length) then begin
+                  t.active <- (offset, length) :: t.active;
+                  fun () ->
+                    t.active <- List.filter (fun (o, l) -> o <> offset || l <> length) t.active;
+                    Lwt_condition.broadcast t.c ()
+                end else
+                  fun () -> () in
+              Lwt.catch
+                (fun () ->
+                  f () >>= fun r ->
+                  unlock ();
+                  return r
+                ) (fun e ->
+                  unlock ();
+                  fail e)
+            end in
+          loop ()
+        )
+
+    let make () =
+      let exclusive_lock = (-1L, 0) in
+      let active = [] in
+      let c = Lwt_condition.create () in
+      let m = Lwt_mutex.create () in
+      { exclusive_lock; active; c; m }
+
+  end
+
   type t = {
     primary: Primary.t;
     secondary: Secondary.t;
     primary_block_size: int; (* number of primary sectors per info.sector_size *)
     secondary_block_size: int; (* number of secondary sectors per info.sector_size *)
-    mutable dirty: Extents.t; (* dirty blocks *)
-    mutable synchronous: bool; (* true if we've switched to synchronous replication *)
     info: info;
+    lock: Region_lock.t;
+    result: [ `Ok of unit | `Error of error ] Lwt.t;
+    mutable percent_complete: int;
     mutable disconnected: bool;
   }
+
+  let start_copy t u =
+    let block = 1024 in
+    let buffer = Cstruct.create (t.info.sector_size * block) in
+    let rec loop sector =
+      t.percent_complete <- Int64.(to_int (div (mul sector 100L) t.info.size_sectors));
+      if t.disconnected || sector = t.info.size_sectors then begin
+        Lwt.wakeup u (`Ok ());
+        return ()
+      end else begin
+        Region_lock.with_exclusive_lock t.lock sector block
+          (fun () ->
+            Primary.read t.primary Int64.(mul sector (of_int t.primary_block_size)) [ buffer ]
+            >>= function
+            | `Error e ->
+              Lwt.wakeup u (`Error e);
+              return ()
+            | `Ok () ->
+              Secondary.write t.secondary Int64.(mul sector (of_int t.secondary_block_size)) [ buffer ]
+              >>= function
+              | `Error e ->
+                Lwt.wakeup u (`Error e);
+                return ()
+              | `Ok () ->
+                loop Int64.(add sector (of_int block))
+          )
+      end in
+    loop 0L
 
   type id = unit
 
@@ -89,30 +212,35 @@ module Make(Primary: V1_LWT.BLOCK)(Secondary: V1_LWT.BLOCK) = struct
       let read_write = primary_info.Primary.read_write in
       let size_sectors = Int64.(div primary_bytes (of_int sector_size)) in
       let info = { read_write; sector_size; size_sectors } in
-      let dirty = Extents.empty in
-      let synchronous = false in
-      return (`Ok { primary; secondary; primary_block_size; secondary_block_size; info; disconnected; dirty; synchronous })
+      let lock = Region_lock.make () in
+      let result, u = Lwt.task () in
+      let percent_complete = 0 in
+      let t = { primary; secondary; primary_block_size; secondary_block_size; info; lock; result; percent_complete; disconnected } in
+      let _ = start_copy t u in
+      return (`Ok t)
 
   let read t ofs bufs =
     let primary_ofs = Int64.(mul ofs (of_int t.primary_block_size)) in
     Primary.read t.primary ofs bufs
 
-  let write t ofs bufs = match t.synchronous with
-    | true -> return (`Error `Unimplemented)
-    | false ->
-      let primary_ofs = Int64.(mul ofs (of_int t.primary_block_size)) in
-      Primary.write t.primary primary_ofs bufs
-      >>= function
-      | `Error e -> return (`Error e)
-      | `Ok () ->
-        let total_length_bytes = List.(fold_left (+) 0 (map Cstruct.len bufs)) in
-        let length = total_length_bytes / t.info.sector_size in
-        let start = Int64.(div ofs (of_int t.info.sector_size)) in
-        t.dirty <- Extents.add ~start ~length t.dirty;
-        return (`Ok ())
+  let write t ofs bufs =
+    let total_length_bytes = List.(fold_left (+) 0 (map Cstruct.len bufs)) in
+    let length = total_length_bytes / t.info.sector_size in
+    let primary_ofs = Int64.(mul ofs (of_int t.primary_block_size)) in
+    let secondary_ofs = Int64.(mul ofs (of_int t.secondary_block_size)) in
+    Region_lock.with_lock t.lock ofs length
+      (fun () ->
+        Primary.write t.primary primary_ofs bufs
+        >>= function
+        | `Error e -> return (`Error e)
+        | `Ok () ->
+          Secondary.write t.secondary secondary_ofs bufs
+      )
 
   let disconnect t =
     t.disconnected <- true;
+    t.result
+    >>= fun _ ->
     return ()
 end
 
