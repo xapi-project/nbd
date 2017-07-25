c3c38b8e658015f80d196862845b7f3ba12f2965
Marcello Seri <marcello.seri@citrix.com>
2017-07-25 14:03:05 +0100

    First attempt to fix the bisect report generation
    Signed-off-by: Marcello Seri <marcello.seri@citrix.com>

--- .coverage.sh
+++ .coverage.sh
@@ -6,22 +6,19 @@ COVERAGE_DIR=.coverage
 rm -rf $COVERAGE_DIR
 mkdir -p $COVERAGE_DIR
 pushd $COVERAGE_DIR
-if [ -z "$KEEP" ]; then trap "popd; rm -rf $COVERAGE_DIR" EXIT; fi
 
 $(which cp) -r ../* .
 
 export COVERAGE=1
 jbuilder runtest
 
-mkdir _outs
-$(find . | grep bisect.*.out | xargs mv -t _outs)
-bisect-ppx-report -I _outs -text report _outs/bisect*.out
-bisect-ppx-report -I _outs -summary-only -text summary _outs/bisect*.out
-(cd _outs; bisect-ppx-report bisect*.out -html ../report-html)
+outs=$(find . | grep bisect.*.out)
+bisect-ppx-report -I $(dirname $outs[1]) -text report $outs
+bisect-ppx-report -I $(dirname $outs[1]) -summary-only -text summary $outs
 
 if [ -n "$TRAVIS" ]; then
   echo "\$TRAVIS set; running ocveralls and sending to coveralls.io..."
-  ocveralls --prefix _outs bisect*.out --send
+  ocveralls --prefix $(dirname $outs[1]) $outs --send
 else
   echo "\$TRAVIS not set; displaying results of bisect-report..."
   cat report
