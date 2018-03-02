a0fc071e208d8a52fa66e110ea47cbc22474c381
Gabor Igloi <gabor.igloi@citrix.com>
2018-03-02 11:50:15 +0000

    Client_server_test: factor out function for testing client/server interaction
    Signed-off-by: Gabor Igloi <gabor.igloi@citrix.com>

--- lib_test/client_server_test.ml
+++ lib_test/client_server_test.ml
@@ -37,17 +37,13 @@ let with_channels f =
   in
   Lwt_unix.with_timeout 0.5 (fun () -> f client_channel server_channel)
 
-let test_connect_disconnect () =
+(** Run the given server and client test sequences concurrently with channels
+    connecting the server and the client together. *)
+let test ~server ~client () =
   Lwt_log.add_rule "*" Lwt_log.Debug;
-  let test_block = (Cstruct.of_string "asdf") in
   let t =
     with_channels (fun client_channel server_channel ->
-        let test_server =
-          Nbd.Server.connect server_channel () >>= fun (export_name, svr) ->
-          Alcotest.(check string) "export name received by server"
-            "export1" export_name;
-          Nbd.Server.serve svr ~read_only:false (module Cstruct_block.Block) test_block
-        in
+        let test_server = server server_channel in
         let cancel, _ = Lwt.task () in
         let test_server =
           Lwt.catch
@@ -55,11 +51,7 @@ let test_connect_disconnect () =
             (function Lwt.Canceled -> Lwt.return_unit | e -> Lwt.fail e)
         in
         let test_client =
-          Nbd.Client.negotiate client_channel "export1" >>= fun (t, size, flags) ->
-          Alcotest.(check int64) "size received by client"
-            (Int64.of_int (Cstruct.len test_block))
-            size;
-          Nbd.Client.disconnect t
+          client client_channel
           (* TODO: because Client.disconnect does not send NBD_CMD_DISC,
              the server loop will not stop - we have to stop it manually.
              Once this is fixed, this cancel mechanism should be removed. *)
@@ -69,6 +61,23 @@ let test_connect_disconnect () =
       )
   in
   Lwt_main.run t
+
+let test_connect_disconnect =
+  let test_block = (Cstruct.of_string "asdf") in
+  test
+    ~server:(fun server_channel ->
+        Nbd.Server.connect server_channel () >>= fun (export_name, svr) ->
+        Alcotest.(check string) "export name received by server"
+          "export1" export_name;
+        Nbd.Server.serve svr ~read_only:false (module Cstruct_block.Block) test_block
+      )
+    ~client:(fun client_channel ->
+        Nbd.Client.negotiate client_channel "export1" >>= fun (t, size, flags) ->
+        Alcotest.(check int64) "size received by client"
+          (Int64.of_int (Cstruct.len test_block))
+          size;
+        Nbd.Client.disconnect t
+      )
 
 let tests =
   "Nbd client-server connection tests",
