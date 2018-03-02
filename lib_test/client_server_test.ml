ee7169c686e13ab2f7265c9beff02ad56d25335d
Gabor Igloi <gabor.igloi@citrix.com>
2018-03-02 12:11:29 +0000

    Client_server_test: add read & write test
    Signed-off-by: Gabor Igloi <gabor.igloi@citrix.com>

--- lib_test/client_server_test.ml
+++ lib_test/client_server_test.ml
@@ -10,17 +10,18 @@ let with_channels f =
   let section = Lwt_log_core.Section.make("with_channels") in
   let make_channel name (ic, oc) =
     let read c =
-      Lwt_log.debug_f ~section "%s read: %d" name (Cstruct.len c) >>= fun () ->
       let len = Cstruct.len c in
+      Lwt_log.debug_f ~section "%s read: %d" name len >>= fun () ->
       let b = Bytes.create len in
       Lwt_io.read_into_exactly ic b 0 len >>= fun () ->
       Cstruct.blit_from_bytes b 0 c 0 len;
-      Lwt_log.debug_f ~section "%s read: %d: %s finished" name (Cstruct.len c) (String.escaped (Cstruct.to_string c))
+      Lwt_log.debug_f ~section "%s read: %d: %s finished" name len (String.escaped (Cstruct.to_string c))
     in
     let write c =
-      Lwt_log.debug_f ~section "%s write: %d: %s" name (Cstruct.len c) (String.escaped (Cstruct.to_string c)) >>= fun () ->
-      Lwt_io.write_from_string_exactly oc (Cstruct.to_string c) 0 (Cstruct.len c) >>= fun () ->
-      Lwt_log.debug_f ~section "%s write: %d: %s finished" name (Cstruct.len c) (String.escaped (Cstruct.to_string c))
+      let len = Cstruct.len c in
+      Lwt_log.debug_f ~section "%s write: %d: %s" name len (String.escaped (Cstruct.to_string c)) >>= fun () ->
+      Lwt_io.write_from_string_exactly oc (Cstruct.to_string c) 0 len >>= fun () ->
+      Lwt_log.debug_f ~section "%s write: %d: %s finished" name len (String.escaped (Cstruct.to_string c))
     in
     (write, read)
   in
@@ -62,6 +63,12 @@ let test ~server ~client () =
   in
   Lwt_main.run t
 
+(** We fail the test if an error occurs *)
+let check msg =
+  function
+  | Result.Ok a -> Lwt.return a
+  | Result.Error _ -> Lwt.fail_with msg
+
 let test_connect_disconnect =
   let test_block = (Cstruct.of_string "asdf") in
   test
@@ -79,6 +86,32 @@ let test_connect_disconnect =
         Nbd.Client.disconnect t
       )
 
+let test_read_write =
+  let test_block = (Cstruct.of_string "asdf") in
+  test
+    ~server:(fun server_channel ->
+        Nbd.Server.connect server_channel () >>= fun (export_name, svr) ->
+        Nbd.Server.serve svr ~read_only:false (module Cstruct_block.Block) test_block
+      )
+    ~client:(fun client_channel ->
+        Nbd.Client.negotiate client_channel "export1" >>= fun (t, size, flags) ->
+
+        let buf = Cstruct.create 2 in
+        Nbd.Client.read t 1L [buf] >>= check "1st read failed" >>= fun () ->
+        Alcotest.(check string) "2 bytes at offset 1" "sd" (Cstruct.to_string buf);
+
+        let buf = Cstruct.of_string "12" in
+        Nbd.Client.write t 2L [buf] >>= check "Write failed" >>= fun () ->
+
+        let buf = Cstruct.create 2 in
+        Nbd.Client.read t 2L [buf] >>= check "2nd read failed" >>= fun () ->
+        Alcotest.(check string) "2 modified bytes at offset 2" "12" (Cstruct.to_string buf);
+
+        Nbd.Client.disconnect t
+      )
+
 let tests =
   "Nbd client-server connection tests",
-  [ "test_connect_disconnect", `Quick, test_connect_disconnect ]
+  [ "test_connect_disconnect", `Quick, test_connect_disconnect
+  ; "test_read_write", `Quick, test_read_write
+  ]
