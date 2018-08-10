9cc1eee8a6033d02971d1303deca5ac893450a53
Gabor Igloi <gabor.igloi@citrix.com>
2018-08-10 11:11:01 +0100

    Test that misbehaving clients do not crash the server
    Now make test will take about 30s because of this new stress test.
    
    Signed-off-by: Gabor Igloi <gabor.igloi@citrix.com>

--- cli_test/suite.ml
+++ cli_test/suite.ml
@@ -6,6 +6,7 @@ let with_command ~command ~strict test =
     if strict then failwith ("Command " ^ command ^ " not present")
     else Printf.printf "!!! Skipping test because command %s is not available" command
 
+(* Each test script takes the path to the NBD CLI as its first command line argument *)
 let script name ~requires (cli, strict) =
   with_command
     ~command:requires
@@ -17,26 +18,30 @@ let script name ~requires (cli, strict) =
          (Sys.command (name ^ " " ^ cli))
     )
 
-let cli =
-  let doc = "Path to nbd CLI should be first command-line argument" in
-  Cmdliner.Arg.(required & opt ~vopt:None (some string) None & info ["cli"] ~docv:"CLI" ~doc)
-
-let strict =
-  let doc = {|If present, the test will fail when the required program is not
-              installed. Otherwise the test will simply be skipped.|}
+let opts =
+  let cli =
+    let doc = "Path to nbd CLI should be first command-line argument" in
+    Cmdliner.Arg.(required & opt ~vopt:None (some string) None & info ["cli"] ~docv:"CLI" ~doc)
   in
-  Cmdliner.Arg.(value & flag & info ["strict"] ~env:(env_var "STRICT") ~doc)
-
-let opts = Cmdliner.Term.(const (fun cli strict -> (cli, strict)) $ cli $ strict)
+  let strict =
+    let doc = {|If present, the test will fail when the required program is not
+                installed. Otherwise the test will simply be skipped.|}
+    in
+    Cmdliner.Arg.(value & flag & info ["strict"] ~env:(env_var "STRICT") ~doc)
+  in
+  Cmdliner.Term.(const (fun cli strict -> (cli, strict)) $ cli $ strict)
 
 let () =
   Alcotest.run_with_args
     "Nbd CLI interoperability tests"
     opts
-    [ ("compatibility with qemu-img",
-       ["data copying", `Slow, script "./test-qemu.sh" ~requires:"qemu-img"]
+    [ ("NBD CLI interoperability tests",
+       [ "data copying with qemu-img", `Quick, script "./test-qemu.sh" ~requires:"qemu-img"
+       ; "listing exports with nbd-client", `Quick, script "./test-nbd-client.sh" ~requires:"nbd-client"
+       ]
       )
-    ; ("compatibility with nbd-client",
-       ["listing exports", `Slow, script "./test-nbd-client.sh" ~requires:"nbd-client"]
+    ; ("Stress tests",
+       [ "Misbehaving clients sending random data", `Slow, script "./random_data.sh" ~requires:"nc"
+       ]
       )
     ]
