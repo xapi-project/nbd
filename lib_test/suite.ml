024c0f007008b659ec0fee2fa2ff60eeaf5626e0
lippirk <ben.anson@citrix.com>
2020-07-07 16:28:21 +0100

    maintenance: fix testsuite with alcotest v1.1.0
    Signed-off-by: lippirk <ben.anson@citrix.com>

--- lib_test/suite.ml
+++ lib_test/suite.ml
@@ -1,8 +1,8 @@
-
 let () =
-  Alcotest.run
-    "Nbd library test suite"
-    [ Mux_test.tests
-    ; Protocol_test.tests
+  Lwt_main.run @@
+  Alcotest_lwt.run
+    "Lwt Nbd library test suite"
+    [ Protocol_test.tests
     ; Client_server_test.tests
-    ]
+    ];
+  Alcotest.run "Synchronous Nbd library test suite" [ Mux_test.tests ]
